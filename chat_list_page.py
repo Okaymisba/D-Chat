c8f67d0faecd92aefc7911b0fb81b46dd9da5661
okaymisba <msbahsarfaraz@gmail.com>
2024-11-18 21:00:09 +0500

    Styled Chat List Frame.

--- chat_list_page.py
+++ chat_list_page.py
@@ -7,7 +7,11 @@ from chat_room import chat_room
 def chat_list_frame(parent, add_chatroom_frame):
     chat_list_page = tk.Frame(parent, width=500, height=700)
     chat_list_page.grid(row=0, column=0, sticky="nsew")
-    canvas = tk.Canvas(chat_list_page)
+
+    app_label = tk.Label(chat_list_page, text="D-Chat", font=("Helvetica", 20, "bold"), fg="Black", pady=10)
+    app_label.pack(side="top", fill="x")
+
+    canvas = tk.Canvas(chat_list_page, width=500)
     scrollbar = tk.Scrollbar(chat_list_page, orient="vertical", command=canvas.yview)
     scrollable_frame = tk.Frame(canvas)
 
@@ -19,10 +23,10 @@ def chat_list_frame(parent, add_chatroom_frame):
     canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
     canvas.configure(yscrollcommand=scrollbar.set)
 
-    add_button = tk.Button(canvas, text="+", font=("Helvetica", 15), bg="light green", width=5, bd=0, cursor="hand2",
+    add_button = tk.Button(canvas, text="+", font=("Helvetica", 15), bg="light green", width=5, bd=1, cursor="hand2",
                            relief="solid", activebackground="light green",
                            command=lambda: show_frame(add_chatroom_frame))
-    add_button.place(x=400, y=625)
+    add_button.place(x=400, y=575)
 
     canvas.pack(side="left", fill="both", expand=True)
     scrollbar.pack(side="right", fill="y")
@@ -30,11 +34,16 @@ def chat_list_frame(parent, add_chatroom_frame):
     def create_chat_box(parent, chat_name, topic):
         chat = chat_room(parent, topic, chat_name)
 
-        chat_frame = tk.Frame(scrollable_frame, bg="lightgray", padx=190, pady=5)
-        chat_label = tk.Label(chat_frame, text=chat_name, font=("Arial", 12), bg="lightgray")
+        y_position = len(scrollable_frame.winfo_children()) * 55
 
-        chat_label.pack(side=tk.LEFT, fill=tk.X, expand=True)
-        chat_frame.pack(fill=tk.X, padx=5, pady=2)
+        chat_frame = tk.Frame(scrollable_frame, bg="light green", height=50, width=490)
+        chat_frame.grid(row=y_position, column=0, sticky="ew", padx=3, pady=3)
+
+        chat_label = tk.Label(chat_frame, text=chat_name, font=("Arial", 15), bg="light green", anchor="w")
+        chat_label.place(x=10, y=15)
+
+        scrollable_frame.update_idletasks()
+        canvas.configure(scrollregion=canvas.bbox("all"))
 
         chat_frame.bind("<Button-1>", lambda e: show_frame(chat))
 
